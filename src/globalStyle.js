a6c715b9cb53fc71edb18816c1f915022dd2b8ad
consultitds <consultoriati@digitalsolvers.com>
2024-06-14 17:22:23 -0300

    Criando o Projeto

---
+++ src/globalStyle.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const primaryAccent = '#ff0000';
+
+export default createGlobalStyle`
+  body {
+    height: 100vh;
+    overflow: scroll;
+  }
+  svg {
+    vertical-align: -0.125em;
+  }
+  .btn.primary-accent {
+    border: 1px solid ${primaryAccent};
+    background: #FFF;
+    color: #000;
+    text-align: left;
+    size: 40px;
+    width: 400px;
+    height: 60px;
+  }
+  .form-check-input:checked {
+    background-color: ${primaryAccent};
+    border-color: ${primaryAccent};
+  }
+  .btn-unstyled {
+    border: none;
+    background: none;
+  }
+  .error-modal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    min-width: 100vw;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: rgba(0,0,0,0.2);
+
+    .error-modal-card {
+      background: #FFF;
+      border: 1px solid rgba(0,0,0,0.2);
+      border-radius: 10px;
+      max-width: 30rem;
+      padding: 1rem;
+    }
+    .error-modal-inner {
+      padding: 1rem;
+    }
+  }
+`;
---
+++ src/routes/Landing.js
@@ -0,0 +1,298 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Color from 'color';
+import { useDispatch, useSelector } from 'react-redux';
+import breakpoints from '../utils/breakpoints';
+import Header from '../components/Header';
+import { landingBackgroundImage, landingBackgroundColor } from '../config';
+import { setRequestedMediaPerms, createScene } from '../store/sm';
+import micFill from '../img/mic-fill.svg';
+import videoFill from '../img/camera-video-fill.svg';
+
+function Landing({ className }) {
+  const { mic, camera } = useSelector(({ sm }) => sm.requestedMediaPerms);
+  const dispatch = useDispatch();
+
+  const {
+    connected,
+    loading,
+    error,
+  } = useSelector(({ sm }) => (sm));
+
+  const createSceneIfNotStarted = () => {
+    if (loading === false && connected === false && error === null) {
+      dispatch(createScene());
+    }
+  };
+
+  useEffect(() => {
+    createSceneIfNotStarted();
+  }, []);
+
+  const [skip, setSkip] = useState(false);
+  const redirectToVideoOnConnect = () => {
+    setSkip(true);
+  };
+  const history = useHistory();
+  useEffect(() => {
+    if (skip === true && connected === true) history.push('/metadoctors-dev');
+  }, [connected, skip]);
+
+  return (
+    <div className={className}>
+      <div className="landing-wrapper">
+        <Header />
+        <div className="container d-flex">
+          <div className="landing-container flex-grow-1">
+            <div className="col-12 col-lg-6">
+              <div className="row" style={{ marginBottom: '9px' }}>
+                <div>
+                  <h1 className="fw-bol">Sou uma Pessoa Digital Hiper-realista, criada pela Digital Solvers com o propósito de trazer vida para a Inteligência Artificial Generativa.</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div>
+                  <h4 className="fw-light" style={{ marginBottom: '31px' }}>
+                    Sou um especialista
+                    <b> metaDoctors-DEV </b>
+                    ,
+                    faça-me perguntas e farei meu melhor para respondê-las.
+                  </h4>
+                </div>
+              </div>
+              <div className="row" style={{ marginBottom: '16px' }}>
+                <div>
+                  <div className="form-check form-switch">
+                    <label
+                      className="form-check-label d-flex align-items-center"
+                      htmlFor="micPermSwitch"
+                    >
+                      <input
+                        className={`shadow form-check-input mic-switch switch ${
+                          mic ? 'status-checked' : 'status-unchecked'
+                        }`}
+                        type="checkbox"
+                        role="switch"
+                        id="micPermSwitch"
+                        onChange={() => dispatch(setRequestedMediaPerms({ mic: !mic }))}
+                        checked={mic}
+                      />
+                      <div className="d-block ms-2">
+                        Use seu microfone para que eu possa te ouvir.
+                      </div>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="row" style={{ marginBottom: '20px' }}>
+                <div>
+                  <div className="form-check form-switch">
+                    <label
+                      className="form-check-label d-flex align-items-center"
+                      htmlFor="cameraPermSwitch"
+                    >
+                      <input
+                        className={`shadow form-check-input video-switch switch ${
+                          camera ? 'status-checked' : 'status-unchecked'
+                        }`}
+                        type="checkbox"
+                        role="switch"
+                        id="micPermSwitch"
+                        onChange={() => dispatch(setRequestedMediaPerms({ camera: !camera }))}
+                        checked={camera}
+                      />
+                      <div className="d-block ms-2">
+                        Deixe sua câmera ligada, pois reajo as suas expressões.
+                      </div>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="row" style={{ marginBottom: '10px' }}>
+                {!connected && (
+                  <div
+                    className="button-start button--disabled m-2 "
+                    type="button"
+                  >
+                    <img alt="gif loading" src="https://media.tenor.com/t5DMW5PI8mgAAAAj/loading-green-loading.gif" className="gif-loading" />
+                  </div>
+                )}
+
+                {connected && (
+                  <button
+                    className={`${connected ? 'button-start' : 'button-start button-start--disabled'} m-2`}
+                    type="button"
+                    disabled={!connected}
+                    onClick={redirectToVideoOnConnect}
+                  >
+                    Converse comigo
+                  </button>
+                )}
+              </div>
+              <div className="row">
+                <div>
+                  Acho difícil ouvi-lo quando você está em uma sala barulhenta,
+                  ou quando há outras conversas acontecendo ao seu redor,
+                  então fale comigo de um lugar tranquilo.
+                </div>
+              </div>
+              <div className="col" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Landing.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
+export default styled(Landing)`
+
+  .fw-bol { font-size: 24px!important;}
+
+  .fw-light{ font-size: 16px; }
+
+  .gif-loading {
+    width: 25px;
+    height: 25px;
+  }
+
+  .fw-bol {
+    font-size: 32px;
+  }
+
+  .landing-wrapper {
+    min-height: 100vh;
+
+    background: ${landingBackgroundImage ? `url(${landingBackgroundImage})` : ''} ${landingBackgroundColor ? `${landingBackgroundColor};` : ''};
+    background-size: auto 50%;
+    background-repeat: no-repeat;
+    background-position: bottom center;
+    z-index: 2;
+
+
+    @media (min-width: ${breakpoints.lg}px) {
+      background-size: 60% auto;
+      background-position: right bottom;
+    }
+  }
+
+  .button-start {
+    border: 1px solid rgb(60, 60, 60);
+    border-radius: 32px;
+    padding:16px 32px;
+    background-color: #8AC43F;
+    color: #ffffff;
+    font-weight: 600;
+    margin: 0;
+  }
+
+  .button--disabled {
+    border: 1px solid rgb(60, 60, 60);
+    border-radius: 32px;
+    padding:16px 32px;
+    background-color: #E5E5E5;
+    color: #ABABAB;
+    font-weight: 600;
+    margin: 0;
+    text-align: center;
+    cursor: not-allowed;
+  }
+
+  .button-start--disabled {
+    background-color: #E5E5E5;
+    color: #ABABAB;
+  }
+
+  .landing-container {
+    padding-top: 1rem;
+    display: flex;
+
+    &>div {
+      background-color: ${Color(landingBackgroundColor).alpha(0.5)};
+      backdrop-filter: blur(10px);
+      border: 1px solid rgba(0,0,0,0.1);
+      padding: 1rem;
+      border-radius: 5px;
+
+      @media (min-width: ${breakpoints.lg}px) {
+        border: none;
+      }
+    }
+  }
+  .form-switch .form-check-input {
+    min-width: 7rem;
+    height: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #8AC43F;
+    color: #ffffff;
+
+
+    &.mic-switch::before, &.mic-switch.status-checked::after {
+        background-image: url(${micFill});
+    }
+    &.video-switch::before, &.video-switch.status-checked::after {
+        background-image: url(${videoFill});
+    }
+    &.mic-switch.status-checked::before, &.video-switch.status-checked::before {
+      background-image: none;
+    }
+
+    &.status-unchecked {
+      background-color: #E5E5E5;
+      color: #ABABAB;
+      &::after {
+        content: 'OFF';
+        color: #000;
+        margin-right: 18%;
+      }
+      &::before {
+        background-size: 60%;
+        background-repeat: no-repeat;
+        background-color: rgb(220, 220, 220);
+        background-position: 45% center;
+        content: '';
+        display: block;
+
+        border-radius: 50%;
+
+        height: 80%;
+        margin-left: 5%;
+        aspect-ratio: 1;
+        float: right;
+      }
+    }
+
+    &.status-checked {
+      &::before {
+        content: 'ON';
+        color: #FFF;
+        margin-left: 22%;
+      }
+
+      &::after {
+        background-size: 60%;
+        background-repeat: no-repeat;
+        background-color: #FFF;
+        background-position: 55% center;
+        content: '';
+        display: block;
+
+        border-radius: 50%;
+
+        height: 80%;
+        margin-right: 5%;
+        aspect-ratio: 1;
+        float: right;
+      }
+    }
+  }
+
+`;
