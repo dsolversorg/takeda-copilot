736f213bd634304d816c0a5aa0ebf91460a4ac23
consultitds <consultoriati@digitalsolvers.com>
2024-06-19 16:19:55 -0300

    Alterando o arquivo Landing.js
    Adicionando 3 funções no botão Converse comigo

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -75,7 +75,7 @@ function Landing({ className }) {
                     className={`${connected ? 'button-start' : 'button-start button-start--disabled'} m-2`}
                     type="button"
                     disabled={!connected}
-                    onClick={[redirectToVideoOnConnect][dispatch(setRequestedMediaPerms({ mic: true}))][dispatch(setRequestedMediaPerms({camera: true }))]}
+                    onClick={[redirectToVideoOnConnect][mic=true][camera=true]}
                   >
                     Converse comigo
                   </button>
