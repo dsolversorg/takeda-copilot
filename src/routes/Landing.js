dedf9f7030a0dd61dc42f04fa95e81989103beb7
consultitds <consultoriati@digitalsolvers.com>
2024-06-19 17:58:31 -0300

    Alterando o arquivo Landing.js
    Ajustando o botão Converse comigo

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -10,7 +10,7 @@ import { landingBackgroundImage, landingBackgroundColor } from '../config';
 import { createScene } from '../store/sm';
 
 function Landing({ className }) {
-  const [ camera, mic ] = useSelector(({ sm }) => sm.requestedMediaPerms);
+  const { camera, mic } = useSelector(({ sm }) => sm.requestedMediaPerms);
   const dispatch = useDispatch();
 
   const {
@@ -75,7 +75,7 @@ function Landing({ className }) {
                     className={`${connected ? 'button-start' : 'button-start button-start--disabled'} m-2`}
                     type="button"
                     disabled={!connected}
-                    onClick={redirectToVideoOnConnect ? mic: true ? camera: true}
+                    onClick={ redirectToVideoOnConnect ? mic : true ? camera : true }
                   >
                     Converse comigo
                   </button>
