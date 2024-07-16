c6544d63781f288b05c810905c093de3379c8c0a
consultitds <consultoriati@digitalsolvers.com>
2024-07-16 00:53:55 -0300

    Voltando o scroll

--- src/routes/DPChat.js
+++ src/routes/DPChat.js
@@ -107,7 +107,7 @@ function DPChat({
         {/* middle row */}
         <div
           className="row d-flex justify-content-end align-items-center flex-grow-1 ps-3 pe-3"
-          style={{ overflow: 'hiden' }}
+          style={{ overflow: 'scroll' }}
           ref={ccDisplaRef}
         >
           <div className="col col-md-5 d-flex align-items-end align-items-md-center" style={{ height: `${ccDisplayHeight}px` || 'auto' }}>
