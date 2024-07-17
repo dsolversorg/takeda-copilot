e7b5c17d9a1610c7e31f33c72ebd87d390b63033
ismott <israel.j.mota@hotmail.com>
2024-07-17 11:40:01 -0300

    feat: ajustando botoes

--- src/routes/DPChat.js
+++ src/routes/DPChat.js
@@ -134,22 +134,13 @@ function DPChat({
               <STTFeedback />
             </div>
           </div>
-          {showTranscript === true || micOn === false ? (
-            <div className="row justify-content-start">
-              <div className="col-md-8 col-lg-5 p-3">
-                <div className={`contControl ${connected && !loading && pathname === '/takeda-copilot' ? '' : 'd-none'}`}>
-                  <Controls />
-                </div>
+          <div className="row justify-content-start">
+            <div className="col-md-8 col-lg-5 p-3">
+              <div className={`contControl ${connected && !loading && pathname === '/takeda-copilot' ? '' : 'd-none'}`}>
+                <Controls />
               </div>
             </div>
-          ) :
-            (<div className="row justify-content-start">
-              <div className="col-md-8 col-lg-5 p-3">
-                <div className={`contControl ${connected && !loading && pathname === '/takeda-copilot' ? '' : 'd-none'}`}>
-                  <Controls />
-                </div>
-              </div>
-            </div>)}
+          </div>
         </div>
       </div>
       {connected ? <PersonaVideo /> : null}
