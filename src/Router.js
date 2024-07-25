9b3fdef69784b9ef31fb051a21de6406a2a5b3a5
consultitds <consultoriati@digitalsolvers.com>
2024-07-25 00:05:45 -0300

    Arquivo Router.js- Ajustando link do reconectar

--- src/Router.js
+++ src/Router.js
@@ -65,7 +65,7 @@ function App() {
                   Desculpe pelo transtorno
                 </p>
                 <div className="d-flex justify-content-center mb-4">
-                  <Link to="/" className="btn btn-dark me-2">Reconectar</Link>
+                  <Link to="/takeda-copilot" className="btn btn-dark me-2">Reconectar</Link>
                   <Link to="/" className="btn btn-outline-dark">Retornar para a página inicial</Link>
                 </div>
                 <div className="d-flex justify-content-center">
