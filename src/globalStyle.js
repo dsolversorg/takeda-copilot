dad21cf3daf178d31dc3f1cd27a39e7886364eda
consultitds <consultoriati@digitalsolvers.com>
2024-07-28 00:28:01 -0300

    Ajustando tamanho do botão

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -30,7 +30,7 @@ export default createGlobalStyle`
     background: #169ca9;
     color: #FFF;
     text-align: center;
-    width: 300px;
+    width: 250px;
     height: auto;
     font-size: 15px;
   }
