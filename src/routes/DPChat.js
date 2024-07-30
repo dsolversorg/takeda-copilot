6f3eddf46bce20723d1fc9f3403e99061bfbcd71
consultitds <consultoriati@digitalsolvers.com>
2024-07-30 10:24:02 -0300

    Ajustando a altura dos botões

--- src/routes/DPChat.js
+++ src/routes/DPChat.js
@@ -270,7 +270,7 @@ export default styled(DPChat)`
     flex-direction: column;
     @media (max-width: 768px){
       position: relative;
-      bottom: 35px;
+      bottom: 30px;
     }        
   }
   
