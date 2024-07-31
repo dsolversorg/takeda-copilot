baac7746c9f7bd43addca7fe3c878a8c4b6db1bf
ismott <israel.j.mota@hotmail.com>
2024-07-31 13:11:16 -0300

    feat: testando desconectar e reconectar

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -26,7 +26,7 @@ function Landing({ className }) {
 
   useEffect(() => {
     console.log('load: ', loading, ' connect: ', connected, ' error: ', error);
-    if (loading === true && connected === true && error === null) {
+    if (loading === false && connected === true && error === null) {
       console.log('entrou');
       dispatch(disconnect());
     }
