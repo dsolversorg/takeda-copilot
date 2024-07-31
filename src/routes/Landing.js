a5f2ef338bba35443aa76a8de6cc3219cb3ec548
ismott <israel.j.mota@hotmail.com>
2024-07-31 13:21:34 -0300

    feat: testando desconectar e reconectar

--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
@@ -27,8 +27,9 @@ function Landing({ className }) {
 
   useEffect(() => {
     console.log('load: ', loading, ' connect: ', connected, ' error: ', error);
-    if (connected)
+    if (connected){
       setBool(true);
+    }
     if (bool) {
       console.log('entrou');
       dispatch(disconnect());
