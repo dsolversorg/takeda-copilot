4314de6e9266e77a450000fc1d281e0188223f05
consultitds <consultoriati@digitalsolvers.com>
2024-08-02 11:17:23 -0300

    Ajustando o botão encerrar

--- src/components/Header.js
+++ src/components/Header.js
@@ -94,7 +94,7 @@ function Header({
                           <button
                             className="btn-unstyled"
                             type="button"
-                            onClick={() => { history.push('/'); }}
+                            onClick={() => { history.push('/takeda-copilot'); }}
                           >
                             <Escape size={20} />
                             {' '}
